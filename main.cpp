8604da0d38014942b9dc887acab31fd17e0fde8a
robin <robin@derenty.net>
2024-12-04 10:49:51 +0100

    feat : add main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "controller/AbstractController.hpp"
+#include "controller/ConsoleController.hpp"
+#include "controller/GUIController.hpp"
+#include "core/GridFactory.hpp"
+
+using namespace std;
+
+int main(int argv, char **argc)
+{
+    return 0;
+}
