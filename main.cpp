700df762a161bc5989407c7983abda3760e02693
robin <robin@derenty.net>
2024-12-04 11:17:06 +0100

    feat : add the gestion of 2 Console and GUI mod with arguments

--- main.cpp
+++ main.cpp
@@ -7,7 +7,28 @@
 
 using namespace std;
 
-int main(int argv, char **argc)
+int main(int argc, char *argv[])
 {
+    Controller::AbstractController* controller;
+
+    if (argc > 1 && (strcmp(argv[1],"-c") == 0))
+    {
+        if (argc > 2)
+        {
+            controller = new Controller::ConsoleController(argv[2]);
+        }
+        else
+        {
+            std::cout << "What is the path to initial file ?" << endl;
+            std::string filePath;
+            cin >> filePath;
+            controller = new Controller::ConsoleController(filePath);
+        }
+    }
+    else
+    {
+        controller = new Controller::GUIController();
+    }
+    controller->run();
     return 0;
 }
