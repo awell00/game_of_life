9d1b02236c4ef13eeea938d898705723f604d7b4
robin <robin@derenty.net>
2024-12-04 10:49:33 +0100

    feat : add Params class

---
+++ core/Params.cpp
@@ -0,0 +1,39 @@
+//
+// Created by Robin Derenty on 03/12/2024.
+//
+
+#include "Params.hpp"
+
+namespace Utils
+{
+    Params::Params(int m_height, int m_width, int m_maxIterations, Grid::GridType m_gridType, int m_exactNeighborToBorn, int m_minNeighborToDie, int m_maxNeighborToDie, int m_iterationDelay)
+    {
+        this->height = m_height;
+        this->width = m_width;
+        this->maxIterations = m_maxIterations;
+        this->gridType = m_gridType;
+        this->exactNeighborToBorn = m_exactNeighborToBorn;
+        this->minNeighborToDie = m_minNeighborToDie;
+        this->maxNeighborToDie = m_maxNeighborToDie;
+        this->iterationDelay = m_iterationDelay;
+    }
+
+    int Params::getHeight() const {return this->height;}
+    int Params::getWidth() const {return this->width;}
+    int Params::getMaxIterations() const {return this->maxIterations;}
+    Grid::GridType Params::getGridType() const {return this->gridType;}
+    int Params::getExactNeighborToBorn() const {return this->exactNeighborToBorn;}
+    int Params::getMinNeighborToDie() const {return this->minNeighborToDie;}
+    int Params::getMaxNeighborToDie() const {return this->maxNeighborToDie;}
+    int Params::getIterationDelay() const{return this->iterationDelay;}
+
+
+
+
+
+
+
+
+
+
+}
---
+++ core/Params.hpp
@@ -0,0 +1,36 @@
+//
+// Created by Robin Derenty on 03/12/2024.
+//
+
+#ifndef PARAMS_HPP
+#define PARAMS_HPP
+#include "AbstractGrid.hpp"
+
+
+namespace Utils
+{
+    class Params {
+    private:
+        int height;
+        int width;
+        int maxIterations;
+        Grid::GridType gridType;
+        int exactNeighborToBorn;
+        int minNeighborToDie;
+        int maxNeighborToDie;
+        int iterationDelay;
+    public:
+        Params(int m_height, int m_width, int m_maxIterations, Grid::GridType m_gridType, int m_exactNeighborToBorn, int m_minNeighborToDie, int m_maxNeighborToDie, int m_iterationDelay);
+        int getHeight() const;
+        int getWidth() const;
+        int getMaxIterations() const;
+        Grid::GridType getGridType() const;
+        int getExactNeighborToBorn() const;
+        int getMinNeighborToDie() const;
+        int getMaxNeighborToDie() const;
+        int getIterationDelay() const;
+    };
+}
+
+
+#endif //PARAMS_HPP
