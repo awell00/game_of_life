4d2e4bf1e35aa9717987852838342582a7cc4251
robin <robin@derenty.net>
2024-12-04 10:49:21 +0100

    feat : add AbstractView class prototype

---
+++ view/AbstractView.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Robin Derenty on 03/12/2024.
+//
+
+#include "AbstractView.hpp"
+
+namespace View
+{
+    AbstractView::AbstractView(){}
+    bool AbstractView::isRunning(){return running;}
+}
---
+++ view/AbstractView.hpp
@@ -0,0 +1,29 @@
+//
+// Created by Robin Derenty on 03/12/2024.
+//
+
+#ifndef VIEW_HPP
+#define VIEW_HPP
+#include "../controller/AbstractController.hpp"
+
+namespace Controller
+{
+    class AbstractController;
+}
+
+namespace View{
+    class AbstractView {
+    protected:
+        bool running;
+        Controller::AbstractController* controller;
+        AbstractView();
+    public:
+        virtual void render() = 0;
+        bool isRunning();
+        virtual ~AbstractView() = default;
+    };
+}
+
+
+
+#endif //VIEW_HPP
